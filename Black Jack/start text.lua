55c3052b2ed76049da3cbae6e53efd127409e9a7
BrandonGaming <47640783+BrandonYeung@users.noreply.github.com>
2019-06-11 13:29:03 -0400

    Add files via upload

---
+++ Black Jack/start text.lua
@@ -0,0 +1,48 @@
+--This is the connection to the main.lua scene changer
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+--This is the change and transition to the next scene after the user presses a button
+local function showScene2 ()
+
+    local options = {
+        effect = "crossFade",
+        time = 100
+    }
+    composer.gotoScene( "menu", options)
+end
+ 
+--The main functionality for the scene is located in the scene:show function
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        
+        --Cosmetics such as the background and title text
+        local background = display.newImageRect ( "Assets/blackjacktext.jpg", 600, 600)
+        background.x = 160
+        background.y = 240
+        background:setFillColor(255, 255, 255)
+        sceneGroup:insert(background)
+
+        
+
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+         timer.performWithDelay( 5000, showScene2 )
+    end
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+--scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+--scene:addEventListener( "hide", scene )
+--scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
